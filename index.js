4df688abf8e75ccde9122fd4141e78c2dceeb6be
Bryan Tempini <mistatempini@gmail.com>
2022-12-04 19:25:21 -0500

    updated addEmployee function

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function roleQuery() {
 
 function employeeQuery() {
   db.query(
-    "SELECT employee.id AS 'ID', employee.first_name AS 'First Name', employee.last_name AS 'Last name', employee.role_id AS 'Role ID', employee.manager_id AS 'Manager ID' FROM employee",
+    "SELECT employee.id AS 'ID', employee.first_name AS 'First Name', employee.last_name AS 'Last name', role.title AS 'Role Title', role.salary AS 'Salary' FROM employee JOIN role ON employee.role_id = role.id",
     function (err, result) {
       console.table(result);
       startApp();
@@ -44,8 +44,28 @@ function employeeQuery() {
 
 function addDepartment(name) {
   db.query(`INSERT INTO department(name) VALUES ("${name.newDepartment}")`);
-  console.log("Success! Your department has been added to the database!");
+  console.log("Success! Your new department has been added to the database!");
   startApp();
+}
+
+function addRole(data) {
+  db.query(
+    `INSERT INTO role(id, title, salary, department_id) VALUES (${data.newRoleID}, "${data.newRole}", ${data.newSalary}, ${data.newDepID})`
+  );
+  console.log("Success! Your new role has been added to the database!");
+  startApp();
+}
+
+function addEmployee(data) {
+  db.query(
+    `INSERT INTO employee(id, first_name, last_name, role_id, manager_id) VALUES (${data.newEmployeeID}, "${data.newEmployeeFN}", "${data.newEmployeeLN}", ${data.newEmployeeRID}, ${data.newEmployeeMID})`
+  );
+  console.log("Success! Your new employee has been added to the database!");
+  startApp();
+}
+
+function updateEmpRole(data) {
+  db.query(``);
 }
 
 //Inquirer Questions
@@ -75,6 +95,57 @@ const addDepartmentQuestions = [
   },
 ];
 
+const addRoleQuestions = [
+  {
+    type: "input",
+    message: "Please enter the role ID#.",
+    name: "newRoleID",
+  },
+  {
+    type: "input",
+    message: "Please enter the name of the role.",
+    name: "newRole",
+  },
+  {
+    type: "input",
+    message: "Please enter the salary.",
+    name: "newSalary",
+  },
+  {
+    type: "input",
+    message: "Please enter the department ID.",
+    name: "newDepID",
+  },
+];
+
+const addEmployeeQuestions = [
+  {
+    type: "input",
+    message: "Please enter the id for the employee you wish to add.",
+    name: "newEmployeeID",
+  },
+  {
+    type: "input",
+    message: "Please enter the first name for the employee you wish to add.",
+    name: "newEmployeeFN",
+  },
+  {
+    type: "input",
+    message: "Please enter the last name for the employee you wish to add.",
+    name: "newEmployeeLN",
+  },
+  {
+    type: "input",
+    message: "Please enter the role id for the employee you wish to add.",
+    name: "newEmployeeRID",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager id for the employee you wish to add.",
+    name: "newEmployeeMID",
+  },
+];
+
 //App
 function startApp() {
   inquirer.prompt(mainQuestion).then((answer) => {
@@ -87,6 +158,14 @@ function startApp() {
     } else if (answer.mainQuestion === "Add a department.") {
       inquirer.prompt(addDepartmentQuestions).then((data) => {
         addDepartment(data);
+      });
+    } else if (answer.mainQuestion === "Add a role.") {
+      inquirer.prompt(addRoleQuestions).then((data) => {
+        addRole(data);
+      });
+    } else if (answer.mainQuestion === "Add an employee.") {
+      inquirer.prompt(addEmployeeQuestions).then((data) => {
+        addEmployee(data);
       });
     }
   });
